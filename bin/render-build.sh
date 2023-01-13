04647268b1cefd6178d37b884c740052a31e222f
Liv Nelson <livnelson@me.com>
2023-01-12 19:25:01 -0800

    Configure for Render

---
+++ bin/render-build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Build commands for front end to create the production build
+rm -rf public
+npm install --prefix client && npm run build --prefix client
+cp -a client/build/. public/
+
+# Build commands for back end
+bundle install
+bundle exec rake db:migrate 
+
+# If you have seed data, run this command for the initial deploy only to avoid duplicate records
+bundle exec rake db:seed
