04a7c0928b6438ab85e29b3b74e3271949f9bd25
Liv Nelson <livnelson@me.com>
2023-01-19 13:11:55 -0800

    mostly working

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -63,13 +63,13 @@ function UserProfile({ user, setUser }) {
 
   const deleteMovie = (name) => setMyFaves(current => current.filter(p => p.name !== name))
 
-//   const filteredFaves = myFaves.filter((fave) => {
-//     if (fave.user_id === user.id) return true
-// })
+  const filteredFaves = myFaves.filter((fave) => {
+    if (fave.user_id === user.id) return true
+})
 
-  // const mappedFaves = filteredFaves.map(movie => {
-  //   return <FaveMovie key={movie.id} id={movie.id} name={movie.name} poster_path={movie.poster_path} overview={movie.overview} deleteMovie={deleteMovie} />
-  // })
+  const mappedFaves = filteredFaves.map(movie => {
+    return <FaveMovie key={movie.id} id={movie.id} name={movie.name} poster_path={movie.poster_path} overview={movie.overview} deleteMovie={deleteMovie} />
+  })
 
   // if (!user) return  <Login />
 
@@ -88,7 +88,7 @@ function UserProfile({ user, setUser }) {
       <h2 className='fave-greeting'>My Faves List</h2>
       <div className='faves-row'>
         <div  className='faves-row-posters'>
-          {/* {mappedFaves} */}
+          {mappedFaves}
         </div>
       </div>
       {errors ? <div className="errors">{errors}</div> : null}
