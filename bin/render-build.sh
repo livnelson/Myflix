43b2a2ea33714a4881e515ca04aa7edf98f909aa
Liv Nelson <livnelson@me.com>
2023-01-25 10:54:13 -0800

    prepping for deployment

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -9,7 +9,7 @@ cp -a client/build/. public/
 
 # Build commands for back end
 bundle install
-bundle exec rake db:migrate 
+bundle exec rake db:migrate
 
-# If you have seed data, run this command for the initial deploy only to avoid duplicate records
-bundle exec rake db:seed
+# if you have seed data, run this command for the initial deploy only to avoid duplicate records
+bundle exec rake db:seed 
